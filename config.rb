fc7af42f8abd12e67fe950222b674c3cbf77c490
Patrick Patzelt <mail@fampat.de>
2015-04-22 11:53:06 +0200

    SASS-Config added dev/production environment option;

--- config.rb
+++ config.rb
@@ -1,25 +1,24 @@
-# Require any additional compass plugins here.
+# SASS Preprocessor configuration
 
-# Set this to the root of your project when deployed:
+preferred_syntax = :scss
 http_path = "/"
-css_dir = "css"
 sass_dir = "css/scss"
-images_dir = "img"
-javascripts_dir = "js"
+environment = :development
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
+if environment == :development
+    css_dir = "css"
+    images_dir = "img"
+    javascripts_dir = "js"
+    line_comments = true
+    output_style = :expanded
+    sass_options = {:debug_info => true}
+end
 
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = false
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+if environment == :production
+    css_dir = "css"
+    images_dir = "img"
+    javascripts_dir = "js"
+    line_comments = false
+    output_style = :compressed
+    sass_options = {:debug_info => false}
+end
